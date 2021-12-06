10758d5de03f0a12450aca849f45d87f43b1b433
admin <aa@aa.cl>
2021-12-06 10:15:41 -0300

    deploy

---
+++ precache-manifest.b79d1b675b29d31fa4fc7c656c6078e1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67ff1a4ea6a9d1bb055",
+    "url": "/vue-trello/css/app.6ee52ff4.css"
+  },
+  {
+    "revision": "27e9a2379ef9437926ad",
+    "url": "/vue-trello/css/board.3f1e568c.css"
+  },
+  {
+    "revision": "e4424691d23f7e6a4846",
+    "url": "/vue-trello/css/chunk-vendors.d95991d2.css"
+  },
+  {
+    "revision": "8f3955304db2b3d0b1c8",
+    "url": "/vue-trello/css/home.c9aac3a5.css"
+  },
+  {
+    "revision": "39939f9a6be022adcd66",
+    "url": "/vue-trello/css/register.fd4dd361.css"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/vue-trello/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/vue-trello/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/vue-trello/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/vue-trello/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "a7162abe315e254719a08e8aa9931f30",
+    "url": "/vue-trello/index.html"
+  },
+  {
+    "revision": "f67ff1a4ea6a9d1bb055",
+    "url": "/vue-trello/js/app.0b5f8aaa.js"
+  },
+  {
+    "revision": "27e9a2379ef9437926ad",
+    "url": "/vue-trello/js/board.2590618e.js"
+  },
+  {
+    "revision": "e4424691d23f7e6a4846",
+    "url": "/vue-trello/js/chunk-vendors.2a4b50b9.js"
+  },
+  {
+    "revision": "8f3955304db2b3d0b1c8",
+    "url": "/vue-trello/js/home.043502d6.js"
+  },
+  {
+    "revision": "39939f9a6be022adcd66",
+    "url": "/vue-trello/js/register.68ec1065.js"
+  },
+  {
+    "revision": "f503c53c2876849f86b3b9608e19b1a7",
+    "url": "/vue-trello/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-trello/robots.txt"
+  }
+]);
